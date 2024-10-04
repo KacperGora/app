c523d841e685d6d9d29c81690f8a1b8b2c9146c6
KacperGora <kacpergoora@gmail.com>
2024-10-04 13:35:33 +0200

    a

--- Backend/index.ts
+++ Backend/index.ts
@@ -19,9 +19,11 @@ app.use('/event', eventRouter)
 app.get('/', (req, res) => {
   res.send('Good morning Kacperek')
 })
+
 app.get('/elo', (req, res) => {
   res.send('Elo Kacperek co tm')
 })
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
 })
