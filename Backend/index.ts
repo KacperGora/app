ec0f2ed06f3937b660f84a9a36141e61334a992c
KacperGora <kacpergoora@gmail.com>
2024-10-09 13:15:59 +0200

    fix

--- Backend/index.ts
+++ Backend/index.ts
@@ -17,7 +17,7 @@ app.use('/api', apiRouter)
 app.use('/event', eventRouter)
 
 app.get('/', (req, res) => {
-  res.send('amigo siemano!')
+  res.send('test usiemano!')
 })
 
 app.get('/elo', (req, res) => {
